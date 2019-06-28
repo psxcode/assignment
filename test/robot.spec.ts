ca9df54948ccd2a6b59fddabbdc96ab411c219c5
psxcode <psxcode@gmail.com>
2019-06-28 22:35:38 +0200

    add more robot tests

--- test/robot.spec.ts
+++ test/robot.spec.ts
@@ -16,7 +16,7 @@ const finished = async (...emitters: NodeJS.EventEmitter[]) => {
 }
 
 describe('[ Robot ]', () => {
-  it('should work', async () => {
+  it('should go and clean the floors', async () => {
     const data = [
       '2',
       '10 22',
@@ -34,6 +34,76 @@ describe('[ Robot ]', () => {
 
     expect(spy.calls).deep.eq([
       [Buffer.from('=> Cleaned: 4\n')],
+    ])
+  })
+
+  it('should find the answer while cleaning', async () => {
+    const data = [
+      '4',
+      '42 42',
+      'E 11',
+      'N 10',
+      'W 11',
+      'S 10',
+    ]
+    const spy = fn()
+    const r = readable({ eager: true, delayMs: 0, log: readableLog })({ objectMode: false })(data)
+    const w = writable({ log: writableLog })({ objectMode: false })(spy)
+    const t = robot()
+
+    r.pipe(t).pipe(w)
+
+    await finished(w)
+
+    expect(spy.calls).deep.eq([
+      [Buffer.from('=> Cleaned: 42\n')],
+    ])
+  })
+
+  it('should not clean where is clean', async () => {
+    const data = [
+      '5',
+      '255 255',
+      'E 10',
+      'W 10',
+      'E 10',
+      'W 10',
+      'E 10',
+    ]
+    const spy = fn()
+    const r = readable({ eager: true, delayMs: 0, log: readableLog })({ objectMode: false })(data)
+    const w = writable({ log: writableLog })({ objectMode: false })(spy)
+    const t = robot()
+
+    r.pipe(t).pipe(w)
+
+    await finished(w)
+
+    expect(spy.calls).deep.eq([
+      [Buffer.from('=> Cleaned: 11\n')],
+    ])
+  })
+
+  it('should clean one very dirty tile', async () => {
+    const data = [
+      '4',
+      '666 666',
+      'E 0',
+      'N 0',
+      'W 0',
+      'S 0',
+    ]
+    const spy = fn()
+    const r = readable({ eager: true, delayMs: 0, log: readableLog })({ objectMode: false })(data)
+    const w = writable({ log: writableLog })({ objectMode: false })(spy)
+    const t = robot()
+
+    r.pipe(t).pipe(w)
+
+    await finished(w)
+
+    expect(spy.calls).deep.eq([
+      [Buffer.from('=> Cleaned: 1\n')],
     ])
   })
 })
